31fbbce04ffecf29c2d15ed372ad4f31f4b3ecd1
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-05 13:07:56 -0700

    Creates GET endpoint for all users

--- api/server.js
+++ api/server.js
@@ -18,19 +18,24 @@ server.post("/api/users", (req, res) => {
         res.status(201).json(user);
       })
       .catch((err) => {
-        res
-          .status(500)
-          .json({
-            message: "There was an error while saving the user to the database",
-          });
+        res.status(500).json({
+          message: "There was an error while saving the user to the database",
+        });
       });
   }
 });
 
 // [GET] /api/users
 server.get("/api/users", (req, res) => {
-  res.status(200).json({ message: "get users" });
-  console.log("GET");
+  User.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: "The users information could not be retrieved" });
+    });
 });
 
 // [GET] /api/users/:id
